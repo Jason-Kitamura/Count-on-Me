e44e985962a6e97801f528d8d05d48c492ffb61c
Jason-Kitamura <jasonkitamura@gmail.com>
2020-04-15 17:26:58 -0400

    working global context, functional components, and reroute to homepage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import LoginPage from './components/Login/LoginPage';
 import HomePage from './components/Home/HomePage';
 import SettingsPage from './components/Settings/SettingsPage';
 import DashboardPage from './components/Dashboard/DashboardPage';
+import {LoginProvider} from './LoginContext';
 
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -20,39 +21,43 @@ function App() {
   }
   return (
     <Router>
-    {console.log("App.js Rendering....")}
+  
+        {console.log("App.js Rendering....")}
 
-    <div className="App">
-        <div class="wrapper">
-          
-          <div class="sidebar"> <SideBar /> </div>
+        <div className="App">
+            <div class="wrapper">
+              
+              <div class="sidebar"> <SideBar /> </div>
 
-          <div class="container-fluid" style={style.RightSection}>
-                
-                <div id="content" class="row pt-2">
-                    <div class="col-1 d-flex justify-content-start pl-0 align-self-baseline" style={style.ToggleSection}>
-                        {/* <nav class="navbar navbar-expand-lg navbar-light bg-light">  */}
-                              <button type="button" id="sidebarCollapse" class="btn btn-info"> {/* Toggle Button */}
-                                <i class="fas fa-align-left"></i>
-                              </button>
-                        {/* </nav> */}
+              <div class="container-fluid" style={style.RightSection}>
+                    
+                    <div id="content" class="row pt-2">
+                        <div class="col-1 d-flex justify-content-start pl-0 align-self-baseline" style={style.ToggleSection}>
+                            {/* <nav class="navbar navbar-expand-lg navbar-light bg-light">  */}
+                                  <button type="button" id="sidebarCollapse" class="btn btn-info"> {/* Toggle Button */}
+                                    <i class="fas fa-align-left"></i>
+                                  </button>
+                            {/* </nav> */}
+                        </div>
+                    
+                        <div class="col-11 ">
+                        <LoginProvider>
+                          <Route exact path={["/", "/search"]} component={SearchPage} />
+                          <Route path={["/login"]} component={LoginPage} />
+                          <Route path={["/dashboard"]} component={DashboardPage} />
+                          <Route path={["/home"]} component={HomePage} />
+                          <Route path={["/settings"]} component={SettingsPage} />
+                        </LoginProvider>
+
+
+                          {/* <Route path="/settings/:id" component={ProductInfoPage} /> */}
+
+                        </div>
                     </div>
-                
-                    <div class="col-11 ">
-                      <Route exact path={["/", "/search"]} component={SearchPage} />
-                      <Route path={["/login"]} component={LoginPage} />
-                      <Route path={["/dashboard"]} component={DashboardPage} />
-                      <Route path={["/home"]} component={HomePage} />
-                      <Route path={["/settings"]} component={SettingsPage} />
-
-
-                      {/* <Route path="/settings/:id" component={ProductInfoPage} /> */}
-
-                    </div>
-                </div>
-              </div>
+                  </div>
+            </div>
         </div>
-    </div>
+
     </Router>
   );
 }
