0fcdbd13b1a24a3e348c48be37b2ecc637097618
Stephanie Blom <stephblom@hotmail.com>
2020-04-15 11:24:13 -0400

    Dashboard with scroll functions

--- src/components/Dashboard/DashboardPage.js
+++ src/components/Dashboard/DashboardPage.js
@@ -1,9 +1,10 @@
-import React from "react";
-import CoverPhoto from './CoverPhoto';
+import React, {useRef} from "react";
 import UserOptions from './UserOptions';
-import TodoLists from './TodoLists.js';
+import TodoLists from './TodoLists';
 import Followers from './Followers';
 import Following from './Following';
+import CoverPhoto from './CoverPhoto'
+import "./style.css";
 
 function HomePage(){
    const home = {
@@ -13,7 +14,8 @@ function HomePage(){
        backgroundColor:'rgb(230, 126, 34)',
        color:'white',
        fontFamily: "'Noto Sans', sans-serif",
-       borderStyle:'groove'
+       borderStyle:'groove', 
+       paddingTop: '5px'
    }
    const liveData = {
        flex:1,
@@ -26,30 +28,69 @@ function HomePage(){
        padding:'0',
        margin:'4px'
    }
+
+   const scroll = {
+    scrollBehavior: 'smooth'
+   }
+
+
+   const followers = useRef(null)
+   const following = useRef(null)
+
+   function executeScrollToFollowers(){
+    console.log(`Calling scroll function`, followers)
+    
+    window.scrollTo(0, followers.current.offsetTop)  
+   }
+
+   function executeScrollToFollowing (){
+    console.log(`Calling scroll function`, following)
+    
+    window.scrollTo(0, following.current.offsetTop)  
+   }
+
     return (
-        <div>
-        <div>
-            <h3  style={home}>Home Page</h3>
-        </div>
-        <div class='row' style={liveData}>
-            <div class='col-12 col-md-8' style={columns}>
-                <CoverPhoto />
+        <div style={scroll}>
+            <div>
+                <h3  style={home}>User Name</h3>
             </div>
-            <div class='col-12 col-md-3'style={columns} >
-                <UserOptions />
+            <div class='row' style={liveData}>
+                <div class='col-12 col-md-8' style={columns}>
+                    <CoverPhoto />
+                </div>
+                <div class='col-12 col-md-3'style={columns} >
+                    <UserOptions executeScrollToFollowers={executeScrollToFollowers} executeScrollToFollowing={executeScrollToFollowing}/>
+                </div>
             </div>
-        </div>
-        <div class='row' style={liveData}>
-            <TodoLists />
-        </div>
-        <div class='row' style={liveData}>
-            <Followers />
-        </div>
-        <div class='row' style={liveData}>
-            <Following />
-        </div>
+            <div class='row' style={liveData}>
+                <TodoLists />
+            </div>
+            <div class='row' ref={followers} style={liveData}>
+                <Followers />
+            </div>
+            <div class='row' ref={following} style={liveData}>
+                <Following />
+            </div>
         </div>
     );
+
+    // const userNameURL = location.hash.substr(1);
+    // const userNameLocalStorage = createUserName( userCredentials.emailAddress );
+
+    // function createUserName ( email ){
+
+    //     const user = email;
+    //     const iend = user.indexOf("@");
+    //     const userName = user.substring(0 , iend);
+
+    //     return userName; }
+
+    //     if ( userNameURL === userNameLocalStorage ){
+    //         // $('#editInfoIcon').removeAttr('style','display : none');
+    //         $('.fa-edit').removeAttr('style','display : none');
+    //     } 
+
+    // }  check if profile is the user profile 
 
 }
 
--- src/components/Dashboard/UserOptions.js
+++ src/components/Dashboard/UserOptions.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useRef} from 'react';
+ 
+    // General scroll to element function
 
-function Goals() {
+function Goals(props) {
     const card={
         width:'100%',
         margin:'10px',
@@ -12,14 +14,16 @@ function Goals() {
         marginLeft:'auto',
         marginRight:'auto'
     }
+
+    
     return (
         <div class="card" style={card}>
             <div class="card-body">
                 <button class='btn btn-light' style={selectOption}><i class="fas fa-plus"></i>   Add New Goal</button>
                 <br></br>
-                <button class='btn btn-light' style={selectOption}><i class="fas fa-users"></i>   Followers</button>
+                <button class='btn btn-light' style={selectOption} onClick={props.executeScrollToFollowers}><i class="fas fa-users"></i>   Followers</button>
                 <br></br>
-                <button class='btn btn-light' style={selectOption}><i class="fas fa-users"></i>   Following</button>
+                <button class='btn btn-light' style={selectOption} onClick={props.executeScrollToFollowing}><i class="fas fa-users"></i>   Following</button>
             </div>
         </div>
     );
