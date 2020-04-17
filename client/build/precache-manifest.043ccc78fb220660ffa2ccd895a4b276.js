5adae73f4920e6a0fe5259ad167af206dc08f91e
Jason-Kitamura <jasonkitamura@gmail.com>
2020-04-17 18:12:20 -0400

    changed git ignore and deleted public and crs at top level

---
+++ client/build/precache-manifest.043ccc78fb220660ffa2ccd895a4b276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de8670eb9c42fb26d02c413665ed080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c431ab8e1295196b8ba1",
+    "url": "/static/css/main.cda37ba1.chunk.css"
+  },
+  {
+    "revision": "582f45bf0dbfa396728f",
+    "url": "/static/js/2.d4765985.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d4765985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c431ab8e1295196b8ba1",
+    "url": "/static/js/main.7175caeb.chunk.js"
+  },
+  {
+    "revision": "61b291e244cd8fdd0dbd",
+    "url": "/static/js/runtime-main.c14eb290.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9989153572ed172349eb3a40a31dfb84.js"
+  "/precache-manifest.043ccc78fb220660ffa2ccd895a4b276.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
