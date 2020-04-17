c90dd3512e36941758308563c5817325e010b792
koustub manchiraju <koustub.247@gmail.com>
2020-04-17 18:38:59 -0400

    added extension to mongo

--- backend/db/orm.js
+++ backend/db/orm.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-//mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/goalTracker', {useNewUrlParser: true, useUnifiedTopology: true,});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+//mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/goalTracker', {useNewUrlParser: true, useUnifiedTopology: true,});
 mongoose.set('useCreateIndex', true);
 
 // include mongoose models (it will include each file in the models directory)
