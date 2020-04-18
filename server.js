fe1e554aada3f3d4cc0ba624f77e78cd5f829ade
koustub manchiraju <koustub.247@gmail.com>
2020-04-18 13:40:08 -0400

    end points correction for server.js

--- server.js
+++ server.js
@@ -16,10 +16,11 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(cors());
-app.use(express.json());
-app.use(express.static('client/build/'));
-//app.use(express.static(path.join(__dirname, "build")));
+
+//app.use(express.static('client/build/'));
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 // NODE ENDPOINTS
 app.post( '/api/createUser', async ( req, res ) => {
     console.log( 'receving body: ', req.body );
