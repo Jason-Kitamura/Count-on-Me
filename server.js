9cb9139e0217bccf41974d12e4dc0eac532d16e6
koustub manchiraju <koustub.247@gmail.com>
2020-04-18 10:35:39 -0400

    changing the directory to access mongo

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-//app.use(express.static('client/build/'));
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static('client/build/'));
+//app.use(express.static(path.join(__dirname, "build")));
 app.use(express.urlencoded({ extended: false }));
 // NODE ENDPOINTS
 app.post( '/api/createUser', async ( req, res ) => {
