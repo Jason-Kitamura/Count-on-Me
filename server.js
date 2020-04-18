d7f35441e9d7aeccdc93b33ac5245e2624eb03d8
koustub manchiraju <koustub.247@gmail.com>
2020-04-18 12:17:36 -0400

    intial commit

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('client/build/'));
+//app.use(express.static('client/build/'));
 app.use(express.static(path.join(__dirname, "build")));
-//app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 // NODE ENDPOINTS
 app.post( '/api/createUser', async ( req, res ) => {
     console.log( 'receving body: ', req.body );
