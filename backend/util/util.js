dbe6c1d651d19bd0ecddc621715264a45d26ed8d
majid <majid.paktinat@gmail.com>
2020-05-18 13:10:07 -0400

    util updated

--- backend/util/util.js
+++ backend/util/util.js
@@ -8,3 +8,17 @@ function getDuplicateArrayElements(arr){
     }
     return results;
 }
+function tree (arr) {
+    if (!arr) return "";
+     if (arr.length === 0) return "";    
+     const sum = (arr, idx) => {
+         if (idx - 1 < arr.length) {
+             if (arr[idx - 1] === -1) return 0;
+             return arr[idx - 1] + sum(arr, idx * 2) + sum(arr, idx * 2 + 1);
+         }
+         return 0;
+     };
+     const left = sum(arr, 2);
+     const right = sum(arr, 3);
+     return (left == right) ? "" : (left > right ? "Left" : "Right"); 
+}
